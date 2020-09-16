a66c13b2ac26279f57e1f3a1d75928dcf08ac54a
Artemy Krupsky <4xel1337@gmail.com>
2020-09-16 14:56:41 +0300

    Добавьте файлы проекта.

---
+++ 4_lab_db/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace _4_lab_db
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.author = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.genre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pubHouse = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.year = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.number = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.textBoxAuthor = new System.Windows.Forms.TextBox();
+            this.textBoxGenre = new System.Windows.Forms.TextBox();
+            this.textBoxPubhouse = new System.Windows.Forms.TextBox();
+            this.textBoxYear = new System.Windows.Forms.TextBox();
+            this.textBoxNum = new System.Windows.Forms.TextBox();
+            this.buttonChange = new System.Windows.Forms.Button();
+            this.numericUpDownId = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownId)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.author,
+            this.genre,
+            this.pubHouse,
+            this.year,
+            this.number});
+            this.dataGridView1.Location = new System.Drawing.Point(1, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(643, 218);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // id
+            // 
+            this.id.HeaderText = "id";
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            // 
+            // author
+            // 
+            this.author.HeaderText = "author";
+            this.author.Name = "author";
+            this.author.ReadOnly = true;
+            // 
+            // genre
+            // 
+            this.genre.HeaderText = "genre";
+            this.genre.Name = "genre";
+            this.genre.ReadOnly = true;
+            // 
+            // pubHouse
+            // 
+            this.pubHouse.HeaderText = "publishing house";
+            this.pubHouse.Name = "pubHouse";
+            this.pubHouse.ReadOnly = true;
+            // 
+            // year
+            // 
+            this.year.HeaderText = "year of issue";
+            this.year.Name = "year";
+            this.year.ReadOnly = true;
+            // 
+            // number
+            // 
+            this.number.HeaderText = "number";
+            this.number.Name = "number";
+            this.number.ReadOnly = true;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(346, 250);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(97, 39);
+            this.buttonAdd.TabIndex = 2;
+            this.buttonAdd.Text = "Add record";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(16, 250);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(97, 39);
+            this.buttonDelete.TabIndex = 3;
+            this.buttonDelete.Text = "Delete record";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // textBoxAuthor
+            // 
+            this.textBoxAuthor.Location = new System.Drawing.Point(142, 224);
+            this.textBoxAuthor.Name = "textBoxAuthor";
+            this.textBoxAuthor.Size = new System.Drawing.Size(96, 20);
+            this.textBoxAuthor.TabIndex = 5;
+            // 
+            // textBoxGenre
+            // 
+            this.textBoxGenre.Location = new System.Drawing.Point(244, 224);
+            this.textBoxGenre.Name = "textBoxGenre";
+            this.textBoxGenre.Size = new System.Drawing.Size(96, 20);
+            this.textBoxGenre.TabIndex = 6;
+            // 
+            // textBoxPubhouse
+            // 
+            this.textBoxPubhouse.Location = new System.Drawing.Point(346, 224);
+            this.textBoxPubhouse.Name = "textBoxPubhouse";
+            this.textBoxPubhouse.Size = new System.Drawing.Size(96, 20);
+            this.textBoxPubhouse.TabIndex = 7;
+            // 
+            // textBoxYear
+            // 
+            this.textBoxYear.Location = new System.Drawing.Point(448, 224);
+            this.textBoxYear.Name = "textBoxYear";
+            this.textBoxYear.Size = new System.Drawing.Size(94, 20);
+            this.textBoxYear.TabIndex = 8;
+            // 
+            // textBoxNum
+            // 
+            this.textBoxNum.Location = new System.Drawing.Point(546, 224);
+            this.textBoxNum.Name = "textBoxNum";
+            this.textBoxNum.Size = new System.Drawing.Size(96, 20);
+            this.textBoxNum.TabIndex = 9;
+            // 
+            // buttonChange
+            // 
+            this.buttonChange.Location = new System.Drawing.Point(141, 250);
+            this.buttonChange.Name = "buttonChange";
+            this.buttonChange.Size = new System.Drawing.Size(97, 39);
+            this.buttonChange.TabIndex = 10;
+            this.buttonChange.Text = "Change record...";
+            this.buttonChange.UseVisualStyleBackColor = true;
+            this.buttonChange.Click += new System.EventHandler(this.buttonChange_Click);
+            // 
+            // numericUpDownId
+            // 
+            this.numericUpDownId.Location = new System.Drawing.Point(16, 224);
+            this.numericUpDownId.Name = "numericUpDownId";
+            this.numericUpDownId.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownId.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 297);
+            this.Controls.Add(this.numericUpDownId);
+            this.Controls.Add(this.buttonChange);
+            this.Controls.Add(this.textBoxNum);
+            this.Controls.Add(this.textBoxYear);
+            this.Controls.Add(this.textBoxPubhouse);
+            this.Controls.Add(this.textBoxGenre);
+            this.Controls.Add(this.textBoxAuthor);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownId)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn author;
+        private System.Windows.Forms.DataGridViewTextBoxColumn genre;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.DataGridViewTextBoxColumn pubHouse;
+        private System.Windows.Forms.DataGridViewTextBoxColumn year;
+        private System.Windows.Forms.DataGridViewTextBoxColumn number;
+        private System.Windows.Forms.TextBox textBoxAuthor;
+        private System.Windows.Forms.TextBox textBoxGenre;
+        private System.Windows.Forms.TextBox textBoxPubhouse;
+        private System.Windows.Forms.TextBox textBoxYear;
+        private System.Windows.Forms.TextBox textBoxNum;
+        private System.Windows.Forms.Button buttonChange;
+        private System.Windows.Forms.NumericUpDown numericUpDownId;
+    }
+}
+
---
+++ 4_lab_db/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.IO;
+
+namespace _4_lab_db
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.AllowUserToAddRows = false;
+            createDB();
+        }
+        
+        public SQLiteConnection dbConnect;
+        public string command = "";
+        int count;
+
+        private void createDB()
+        {
+
+            dbConnect = new SQLiteConnection("Data Source = library.db; Version = 3");
+
+            if (!File.Exists("./library.db"))
+                SQLiteConnection.CreateFile("library.db");
+
+            command = "CREATE TABLE IF NOT EXISTS [books]([id] INTEGER PRIMARY KEY AUTOINCREMENT, [author] VARCHAR(45), [genre] VARCHAR(45), [publishing house] VARCHAR(45), [year of issue] INTEGER, [number] INTEGER)";
+            SQLiteCommand commandCreate = new SQLiteCommand(command, dbConnect);
+            dbConnect.Open();
+            commandCreate.ExecuteNonQuery();
+            dbConnect.Close();
+
+            dbConnect.Open();
+            command = "INSERT INTO [books]([author], [genre], [publishing house], [year of issue], [number]) VALUES ('jojo', 'golden wind', 'italia', 2000, 5)";
+            SQLiteCommand commandAdd = new SQLiteCommand(command, dbConnect);
+            commandAdd.ExecuteNonQuery();
+            dbConnect.Close();
+
+            dataGridView1.Rows.Clear();
+
+            dbConnect.Open();
+            command = "SELECT * FROM books";
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(command, dbConnect);
+
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+
+            if (table.Rows.Count > 0)
+            {
+                for (int i = 0; i < table.Rows.Count; i++)
+                {
+                    dataGridView1.Rows.Add(table.Rows[i].ItemArray);
+                    count = i;
+                }
+                    
+            }
+            dbConnect.Close();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            dbConnect.Open();
+            string commandInsert = "INSERT INTO [books]([author], [genre], [publishing house], [year of issue], [number]) VALUES ('"+ textBoxAuthor.Text +"', '"+ textBoxGenre.Text +"', '"+ textBoxPubhouse.Text +"', "+ textBoxYear.Text + ", " + textBoxNum.Text + ")";
+            SQLiteCommand addCommand = new SQLiteCommand(commandInsert, dbConnect);
+            addCommand.ExecuteNonQuery();
+
+            command = "SELECT * FROM books";
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(command, dbConnect);
+
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+
+            //if (table.Rows.Count > 0)
+            //{
+            //    for (int i = 0; i < table.Rows.Count; i++)
+            //        dataGridView1.Rows.Add(table.Rows[i].ItemArray);
+            //}
+
+            dataGridView1.Rows.Add(table.Rows[++count].ItemArray);
+            dbConnect.Close();
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            dbConnect.Open();
+            command = "DELETE FROM books WHERE id = " + numericUpDownId.Value;
+            SQLiteCommand commandDelete = new SQLiteCommand(command, dbConnect);
+            commandDelete.ExecuteNonQuery();
+
+            command = "SELECT * FROM books";
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(command, dbConnect);
+
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+
+            dataGridView1.Rows.Clear();
+
+            if (table.Rows.Count > 0)
+            {
+                for (int i = 0; i < table.Rows.Count; i++)
+                {
+                    dataGridView1.Rows.Add(table.Rows[i].ItemArray);
+                    count = i;
+                }
+            }
+            dbConnect.Close();
+        }
+
+        private void buttonChange_Click(object sender, EventArgs e)
+        {
+            if (numericUpDownId.Value != 0)
+            { 
+                dbConnect.Open();
+                command = "UPDATE books SET author = '" + textBoxAuthor.Text + "' where id = " + numericUpDownId.Value;
+                SQLiteCommand commandUpdate = new SQLiteCommand(command, dbConnect);
+                commandUpdate.ExecuteNonQuery();
+                dbConnect.Close();
+            }
+        }
+    }
+}
